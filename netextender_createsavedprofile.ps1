90eaebd24032cec9a9b45919faa81497b449e2df
Nick Seratt <nseratt@nskinc.com>
2017-06-26 18:41:16 -0400

    save netextender profile
    create netextender profile without having to connect first

---
+++ netextender_createsavedprofile.ps1
@@ -0,0 +1,37 @@
+# Run powershell as admin then .\netextender_createsavedprofile.ps1 or copy and paste this code in and fill in the prompts.
+
+
+
+# Creates a new NetExtender saved profile without having to connect to the VPN first for it to save the profile.
+# -Nick Seratt
+
+
+$VPNServer = Read-Host -Prompt 'Enter the VPN host (and :port if necessary. ex: 123.45.67.89:4433)'
+$VPNDomain = Read-Host -Prompt 'Enter the VPN Domain (ex: LocalDomain)'
+$VPNUser = Read-Host -Prompt 'Enter the VPN Username (optional)'
+
+
+$VPNRegistry = @"
+Windows Registry Editor Version 5.00
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\SonicWALL\SSL-VPN NetExtender\Standalone\Profiles\1D29F37492AE010]
+"server"="$VPNServer"
+"domain"="$VPNDomain"
+"user"="$VPNUser"
+"np0"=hex:00
+"np1"=hex:d0,2b,28,4a,37,9f,d2,01
+"np2"=hex:d0,2b,28,4a,37,9f,d2,01
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\SonicWALL\SSL-VPN NetExtender\Standalone\Profiles]
+"defaultProfile"="$VPNServer($VPNUser@$VPNDomain)"
+"@
+
+
+$VPNRegistry -replace '\n', "`r`n"  | Out-File $env:TEMP\vpnreg.reg
+
+cmd /C regedit.exe /S $env:TEMP\vpnreg.reg
+
+Remove-Item  $env:TEMP\vpnreg.reg
+
+Stop-Process -processname NEGui
+Restart-Service  SONICWALL_NetExtender
